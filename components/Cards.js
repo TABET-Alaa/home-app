0982fc6a1c112691bfc7684a81c970be15b034b6
Alaaddine TABET <alta.tabet@gmail.com>
2022-11-03 15:31:58 +0100

    add the card componenets

---
+++ components/Cards.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Cards({ img , title}) {
+    return (
+        <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+            <div className="relative h-80 w-80">
+                <Image className="rounded-xl" src={img} layout="fill" />
+            </div>
+            <h3 className="text-2xl mt-3"> {title} </h3>
+        </div>
+    
+    )
+}
+
+export default Cards
