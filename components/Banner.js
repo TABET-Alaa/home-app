a5e512a2520950bed4afad057e8836323bcada10
Alaaddine TABET <alta.tabet@gmail.com>
2022-11-03 17:36:49 +0100

    fixed bug

--- components/Banner.js
+++ components/Banner.js
@@ -8,14 +8,14 @@ function Banner() {
             src="https://links.papareact.com/0fm" 
             layout="fill" 
             objectFit="cover"
+            alt="image"
         />
 
         <div className="absolute top-1/2 w-full text-center">
             <p className="text-sm sm:text-lg"> Not sure where to go ? Perfect .</p>
             <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 
-                                hover:shadow-xl active:scale-90 transition duration-150
-            ">
-                I'm flexible
+                                hover:shadow-xl active:scale-90 transition duration-150">
+                I am flexible
             </button>
         </div>
         </div>
--- components/ExploreCard.js
+++ components/ExploreCard.js
@@ -5,7 +5,7 @@ function ExploreCard( {img, location, distance}) {
     return (
         <div className="flex items-center space-x-4 m-2 mt-5 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration:200 ease-out">
             <div className="relative h-16 w-16">
-                <Image src={img} layout="fill" className="rounded-lg"/>
+                <Image src={img} layout="fill" className="rounded-lg " alt="image"/>
             </div>
 
             <div>
