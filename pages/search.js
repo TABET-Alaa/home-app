d7dd4689f87874b6be335e464c8d946dbf3f297e
Alaaddine TABET <alta.tabet@gmail.com>
2022-11-09 21:48:27 +0100

    add the header and footer to the search page

---
+++ pages/search.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+function search() {
+    return (
+        <div>
+            <Header />
+
+            <main className="flex">
+                
+            </main>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default search
