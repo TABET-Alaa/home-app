d76a4648e8195a41a967d1cdd459d4a301baac18
Alaaddine TABET <alta.tabet@gmail.com>
2022-11-09 21:47:28 +0100

    add the search and cancel option at the header

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 import {    SearchIcon,
             GlobeAltIcon,
             MenuIcon,
-            UserCircleIcon
+            UserCircleIcon,
+            UsersIcon
 } from '@heroicons/react/solid'
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
@@ -13,11 +14,12 @@ function Header() {
     const [searchInput, setSearchInput] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+    const [numberOfGuests, setNumberOfGuests] = useState(1)
 
     const selectionRange = {
         startDate: startDate,
         endDate: endDate,
-        key: 'selection '
+        key: 'selection'
     }
 
     const handleSelect = (ranges) => {
@@ -25,6 +27,10 @@ function Header() {
         console.log("ranges start date : ",ranges.selection)
         setStartDate(ranges.selection.startDate);
         setEndDate(ranges.selection.endDate);
+    }
+
+    const resetInput = () => {
+        setSearchInput('');
     }
 
     return (
@@ -49,13 +55,28 @@ function Header() {
                 </div>
             </div>
             {searchInput && 
-                <div>
+                <div className="flex flex-col col-span-3 mx-auto">
                     <DateRangePicker 
                         ranges={[selectionRange]}
                         minDate={new Date()} 
                         rangeColors={["#FD5B61"]}
                         onChange={handleSelect}
                     />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold ">Number of Guests </h2>
+                        <UsersIcon className="h-5"/>
+                        <input 
+                            type="number" 
+                            className="w-12 pl-2 text-lg outline-none text-red-400"
+                            min={1}
+                            value={numberOfGuests}
+                            onChange={(e) => setNumberOfGuests(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex">
+                        <button className="flex-grow text-gray-500" onClick={resetInput}>Cancel</button>
+                        <button className="flex-grow text-gray-400">Search</button>
+                    </div>
                 </div> 
             }
         </header>
