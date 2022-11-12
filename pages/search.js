11ff61afa7b39a6dadf1e9227a447be603135f1e
Alaaddine TABET <alta.tabet@gmail.com>
2022-11-12 17:04:49 +0100

    name of the main and the first filter button version -1

--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,16 @@ function search() {
             <Header />
 
             <main className="flex">
-                
+                <section>
+                    <p className="text-xs">+50 Stays for 4 nombre of guests</p>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+
+                    <div>
+                        <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">
+                            Cancelation flexibility 
+                        </p>
+                    </div>
+                </section>
             </main>
 
             <Footer />
