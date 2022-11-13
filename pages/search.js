128ae0bf0e71c6ffa758049297f2145017502267
Alaaddine TABET <alta.tabet@gmail.com>
2022-11-13 16:37:38 +0100

    range , numberOfGuests,location extract with the useRouter hook

--- pages/search.js
+++ pages/search.js
@@ -1,21 +1,33 @@
+import { useRouter } from 'next/router';
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-
+import { format } from "date-fns";
 function search() {
+
+    const router = useRouter();
+    const {location, startDate , endDate, numberOfGuests } = router.query;
+    const formatedStartDate = format(new Date(startDate),"dd MMMM yy");
+    const formatedEndDate = format(new Date(endDate),"dd MMMM yy");
+    const range = `${formatedStartDate} -- ${formatedEndDate} `
     return (
         <div>
             <Header />
 
             <main className="flex">
-                <section>
-                    <p className="text-xs">+50 Stays for 4 nombre of guests</p>
-                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+                <section className="flex-grow pt-14 px-6">
+                    <p className="text-xs">+50 Stays - {range}  - for {numberOfGuests} nombre of guests</p>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
 
-                    <div>
-                        <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">
+                    <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                        <p className="button">
                             Cancelation flexibility 
                         </p>
+                        <p className="button">Type of Place</p>
+                        <p className="button">Type of Place</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms And Beds</p>
+                        <p className="button">More filters</p>
                     </div>
                 </section>
             </main>
