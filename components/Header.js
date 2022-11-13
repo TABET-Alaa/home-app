99b5394c8eb35a9912982507da9e871c23f8349f
Alaaddine TABET <alta.tabet@gmail.com>
2022-11-13 17:25:40 +0100

    fix the bug setSearchInput

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
 import { useRouter } from 'next/router';
 
-function Header() {
+function Header({placeholder}) {
     const [searchInput, setSearchInput] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
@@ -59,8 +59,8 @@ function Header() {
             <div className="flex items-center md:border-2 rounded-full p-2">
                 <input 
                     value={searchInput}
-                    onChange={(event) => setSearchInput(event)}
-                    type="text" placeholder="Start your search" className="pl-5 bg-transparent outline-none text-sm text-gray-600 flex-grow md:shadow-sm"/>
+                    onChange={(event) => setSearchInput(event.target.value)}
+                    type="text" placeholder={placeholder || "Start your search"} className="pl-5 bg-transparent outline-none text-sm text-gray-600 flex-grow md:shadow-sm"/>
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
             </div>
             <div className="flex items-center space-x-4 justify-end text-gray-500">
